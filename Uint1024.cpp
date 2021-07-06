b8f4ae750bf5b6b525ddc7a682afb466f8c004fb
Phips04 <65680501+Phips04@users.noreply.github.com>
2021-07-06 13:14:22 +0200

    Delete Uint1024.cpp

--- Uint1024.cpp
+++
@@ -1,7 +0,0 @@
-#include "Uint1024.h"
-
-Uint1024::Uint1024() : BigIntsBase() {}
-Uint1024::Uint1024(std::string Bits) : BigIntsBase(Bits) {}
-Uint1024::Uint1024(std::bitset<1024> array) : BigIntsBase(array) {}
-Uint1024::Uint1024(int Arg) : BigIntsBase(Arg) {}
-Uint1024::Uint1024(uint64_t Arg) : BigIntsBase(Arg) {}
