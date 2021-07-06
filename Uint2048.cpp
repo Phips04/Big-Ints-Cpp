15ca43dee8be35232584a08d6644e45196d57aa6
Phips04 <65680501+Phips04@users.noreply.github.com>
2021-07-06 13:15:35 +0200

    Delete Uint2048.cpp

--- Uint2048.cpp
+++
@@ -1,7 +0,0 @@
-#include "Uint2048.h"
-
-Uint2048::Uint2048() : BigIntsBase() {}
-Uint2048::Uint2048(std::string Bits) : BigIntsBase(Bits) {}
-Uint2048::Uint2048(std::bitset<2048> array) : BigIntsBase(array) {}
-Uint2048::Uint2048(int Arg) : BigIntsBase(Arg) {}
-Uint2048::Uint2048(uint64_t Arg) : BigIntsBase(Arg) {}
