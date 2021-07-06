e5d6f641260b255c9e49fff6a0cc31d9c0930864
Phips04 <65680501+Phips04@users.noreply.github.com>
2021-07-06 13:15:23 +0200

    Delete Uint16384.cpp

--- Uint16384.cpp
+++
@@ -1,7 +0,0 @@
-#include "Uint16384.h"
-
-Uint16384::Uint16384() : BigIntsBase() {}
-Uint16384::Uint16384(std::string Bits) : BigIntsBase(Bits) {}
-Uint16384::Uint16384(std::bitset<16384> array) : BigIntsBase(array) {}
-Uint16384::Uint16384(int Arg) : BigIntsBase(Arg) {}
-Uint16384::Uint16384(uint64_t Arg) : BigIntsBase(Arg) {}
