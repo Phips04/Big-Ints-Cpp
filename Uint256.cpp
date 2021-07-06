30556e2e16f3eb4733a559d6942855d0660f15be
Phips04 <65680501+Phips04@users.noreply.github.com>
2021-07-06 13:15:47 +0200

    Delete Uint256.cpp

--- Uint256.cpp
+++
@@ -1,7 +0,0 @@
-#include "Uint256.h"
-
-Uint256::Uint256() : BigIntsBase() {}
-Uint256::Uint256(std::string Bits) : BigIntsBase(Bits) {}
-Uint256::Uint256(std::bitset<256> array) : BigIntsBase(array) {}
-Uint256::Uint256(int Arg) : BigIntsBase(Arg) {}
-Uint256::Uint256(uint64_t Arg) : BigIntsBase(Arg) {}
