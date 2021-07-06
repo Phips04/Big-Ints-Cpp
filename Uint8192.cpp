e01b46c7a1708beed8b81b0398a3a0d9dfccfd27
Phips04 <65680501+Phips04@users.noreply.github.com>
2021-07-06 13:16:29 +0200

    Delete Uint8192.cpp

--- Uint8192.cpp
+++
@@ -1,7 +0,0 @@
-#include "Uint8192.h"
-
-Uint8192::Uint8192() : BigIntsBase() {}
-Uint8192::Uint8192(std::string Bits) : BigIntsBase(Bits) {}
-Uint8192::Uint8192(std::bitset<8192> array) : BigIntsBase(array) {}
-Uint8192::Uint8192(int Arg) : BigIntsBase(Arg) {}
-Uint8192::Uint8192(uint64_t Arg) : BigIntsBase(Arg) {}
